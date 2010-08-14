a3c37601e0c01af92a10a9c02b21672b0378fe6c
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-14 05:41:01 -0500

    hg to git on the server is done

--- repocracy/repo/tasks.py
+++ repocracy/repo/tasks.py
@@ -1,15 +1,58 @@
 import os
 import subprocess
+import sys
 
 from django.conf import settings
 from celery.decorators import task
 
-from repocracy.repo.models import Repository
+from repocracy.repo.models import Repository, Status, RepoTypes
+import mercurial.ui
+import mercurial.localrepo
+import hggit
 
 @task
 def translate_repository(repo_pk):
-    pass
+    try:
+        repo = Repository.objects.get(pk=repo_pk) 
+    except Repository.DoesNotExist:
+        pass
+    else:
+        if repo.origin_type == RepoTypes.HG:
 
+            hgui = mercurial.ui.ui()
+            hgui.setconfig('git', 'intree', 'true')
+            hgui.setconfig('git', 'exportbranch', 'refs/heads/master')
+
+            hgpath = os.path.join(repo.fs_path, 'hg') 
+            hgrepo = mercurial.localrepo.localrepository(hgui, hgpath)
+            githandler = hggit.GitHandler(hgrepo, hgui)
+            githandler.export_commits()
+
+            tip_changeset = hgrepo['tip']
+            tip_hex = tip_changeset.hex()
+            
+            hgrepopath = os.path.join(hgpath, '.hg')
+            hggitmap = os.path.join(hgrepopath, 'git-mapfile')
+            
+            gitrepopath = os.path.join(hgpath, '.git')
+            githead = os.path.join(gitrepopath, 'refs', 'heads', 'master')
+
+            with open(hggitmap, 'r') as input:
+                for line in input:
+                    githash, hghash = line.split(' ')
+                    hghash = hghash.strip()
+                    if hghash == tip_hex:
+                        with open(githead, 'w') as output:
+                            output.write(githash)
+                        break
+            result = subprocess.call(
+                args=['git','clone','--mirror', hgpath, './'],
+                cwd=os.path.join(repo.fs_path, 'git')
+            )
+        elif repo.origin_type == RepoTypes.GIT:
+            pass
+        else:
+            pass
 @task
 def clone_repository(repo_pk):
     try:
@@ -24,22 +67,27 @@ def clone_repository(repo_pk):
         destination_dirs = [os.path.join(destination, type) for type in ('hg', 'git')]
         for i in destination_dirs:
             os.makedirs(i)
-        result = subprocess.call(
-            args=['git', '--git-dir=.', 'clone', repo.origin, '.'],
-            cwd=destination_dirs[1]
+
+        gitproc = subprocess.Popen(
+            args=['git', '--git-dir=.', 'clone', '--mirror', repo.origin, '.'],
+            cwd=destination_dirs[1],
+            stdin=subprocess.PIPE,
+            close_fds=True
         )
+        result = gitproc.wait()
         if result != 0:
             result = subprocess.call(
                 args=['hg', 'clone', repo.origin, '.'],
                 cwd=destination_dirs[0]
             )
             if result == 0: 
-                repo.origin_type = 1
-                repo.status = 1
+                repo.origin_type = RepoTypes.HG 
+                repo.status = Status.CLONED 
             else:
-                repo.status = 255
+                repo.status = Status.ERROR 
         else:
-            repo.origin_type = 0
-            repo.status = 1
+            repo.origin_type = RepoTypes.GIT 
+            repo.status = Status.CLONED
+        repo.fs_path = destination 
         repo.save()
         translate_repository.delay(repo.pk)
