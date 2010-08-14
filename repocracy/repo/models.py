2cec0c4f68cdd35e14bb3f0e8e50624fdb66263e
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-14 00:34:36 -0500

    added Repository model

--- repocracy/repo/models.py
+++ repocracy/repo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+REPOTYPES = (
+    (0, 'git'),
+    (1, 'hg'),
+)
+
+class Repository(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    origin = models.CharField(max_length=255)
+    origin_type = models.IntegerField(choices=REPOTYPES)
+    fs_path = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_name(self):
+        username = '' if self.user is None else self.user.username
+        return '/'.join([i for i in [username, self.name] if i])
+
+    def get_absolute_url(self):
+        return reverse('repo_detail', kwargs={
+            'name':slugify(self.get_name()),
+        })
