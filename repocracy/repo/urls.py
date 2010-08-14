2061c6d79137068c9570df9da15074ed048e80d2
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-14 14:34:54 -0500

    add forward slash to match repo urls

--- repocracy/repo/urls.py
+++ repocracy/repo/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('repocracy.repo.views',
     url(r'^$', 'home', name='home'),
     url(r'^claim/(?P<pk>\d+)/(?P<claim_hash>[a-fA-F\d]{40})/$', 'repo_claim', name='repo_claim'),
-    url(r'^repos/(?P<name>[\-_\d\w\\\.]+)/$', 'repo_detail', name='repo_detail'),
+    url(r'^repos/(?P<name>[/\-_\d\w\\\.]+)/$', 'repo_detail', name='repo_detail'),
 )
