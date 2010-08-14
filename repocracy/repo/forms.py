fcf82ffb3331c61c74bff44dbc52711c253a134d
Cody Soyland <codysoyland@gmail.com>
2010-08-14 12:45:01 -0500

    Auto setup repository on form save.

--- repocracy/repo/forms.py
+++ repocracy/repo/forms.py
@@ -1,12 +1,14 @@
 from django.forms import ModelForm
 
 from repocracy.repo.models import Repository
+from repocracy.repo.tasks import clone_repository
 
 class NewRepoForm(ModelForm):
     def save(self):
         obj = super(NewRepoForm, self).save(commit=False)
         obj.name = obj.guess_name()
         obj.save()
+        clone_repository.delay(obj.pk)
         return obj
 
     class Meta:
