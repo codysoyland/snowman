5baf16dc2cdc87d8086e13ec9b5bab20c116eb97
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-08-15 13:52:21 -0500

    initial style commit, woo

--- repocracy/urls.py
+++ repocracy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,3 +9,9 @@ urlpatterns = patterns('',
     ('^bluebird/', include('bluebird.urls')),
     (r'', include('repocracy.repo.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
